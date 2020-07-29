6cff1ff6f715644ceb1bd12b9e010da2dad4f29b
XstrawberryX <ccarl3@icloud.com>
2020-07-29 19:42:20 -0400

    reinitial commit
    no for real this time
    
    print ("Hello world!")

---
+++ projectzero/Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+  [SerializeField] private Transform parent;
+  [SerializeField] private Transform camera;
+  [SerializeField] private float offset = -10;
+  private Vector3 temp = Vector3.zero;
+
+  private void LateUpdate()
+  {
+    temp = new Vector3 (parent.position.x, parent.position.y, parent.position.z + offset);
+    camera.position = temp;
+  }
+}
---
+++ projectzero/Assets/Scripts/Raycast Control/Controller2D.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour
+{
+  private const float skin = .01f;
+  public int horizontalRayCount = 4;
+  public int verticalRayCount = 4;
+
+  private float horizontalRaySpace;
+  private float verticalRaySpace;
+
+  public BoxCollider2D moveCollider;
+  private RaycastOrigins raycastOrigins;
+
+  void Start()
+  {
+    moveCollider = GetComponent<BoxCollider2D>();
+    CalculateRaySpace();
+  }
+
+  public void Move(Vector3 velocity)
+  {
+    UpdateRaycastOrigins();
+
+    VerticalCollisions (ref velocity);
+
+    transform.Translate (velocity);
+  }
+
+  void VerticalCollisions(ref Vector3 velocity)
+  {
+    for (int i = 0; i < verticalRayCount; i++)
+    {
+      Debug.DrawRay (raycastOrigins.bottomLeft + Vector2.right * verticalRaySpace * i, Vector2.up * -2, Color.red);
+    }
+  }
+
+  void UpdateRaycastOrigins()
+  {
+    Bounds bounds = moveCollider.bounds;
+    bounds.Expand (skin * -2);
+
+    raycastOrigins.bottomLeft = new Vector2 (bounds.min.x, bounds.min.y);
+    raycastOrigins.bottomRight = new Vector2 (bounds.max.x, bounds.min.y);
+    raycastOrigins.topLeft = new Vector2 (bounds.min.x, bounds.max.y);
+    raycastOrigins.topRight = new Vector2 (bounds.max.x, bounds.max.y);
+  }
+
+  void CalculateRaySpace()
+  {
+    Bounds bounds = moveCollider.bounds;
+    bounds.Expand (skin * -2);
+
+    horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+    verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+    horizontalRaySpace = bounds.size.y / (horizontalRayCount - 1);
+    verticalRaySpace = bounds.size.x / (verticalRayCount -1);
+  }
+
+  struct RaycastOrigins
+  {
+    public Vector2 topLeft, topRight;
+    public Vector2 bottomLeft, bottomRight;
+  }
+}
